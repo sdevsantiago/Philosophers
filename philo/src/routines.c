c01917b3a3451225b8e6027c3e532c17fd31bf6d
sede-san <sede-san@student.42madrid.com>
2025-09-30 20:50:02 +0200

    update: hunger is now checked in every action

--- philo/src/routines.c
+++ philo/src/routines.c
@@ -6,15 +6,16 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/23 21:50:33 by sede-san          #+#    #+#             */
-/*   Updated: 2025/09/30 14:09:58 by sede-san         ###   ########.fr       */
+/*   Updated: 2025/09/30 20:45:43 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
 static int	routine_eat(t_philo *philo);
-static void	routine_sleep(t_philo *philo);
-static void	routine_think(t_philo *philo);
+static int	routine_sleep(t_philo *philo);
+static int	routine_think(t_philo *philo);
+static int	philo_has_starved(t_philo *philo);
 
 void	*philo_routine(
 	void *arg)
@@ -25,12 +26,14 @@ void	*philo_routine(
 	philo->timestamp_death = *philo->timestamp_start + *philo->time_to_die;
 	while (!philo->dead_philo)
 	{
-		if (routine_eat(philo) == PHILO_STARVES)
+		if (routine_eat(philo) == PHILO_DIES)
 			break ;
 		else if (!philo->meals_count)
 			return (NULL);
-		routine_sleep(philo);
-		routine_think(philo);
+		else if (routine_sleep(philo) == PHILO_DIES)
+			break ;
+		else if (routine_think(philo) == PHILO_DIES)
+			break ;
 	}
 	return (philo);
 }
@@ -40,18 +43,20 @@ static int	routine_eat(
 {
 	if (philo->forks[LEFT_FORK] == philo->forks[RIGHT_FORK])
 		msleep(*philo->time_to_die);
-	if (get_current_timestamp_ms() >= philo->timestamp_death)
-		return (PHILO_STARVES);
-	if (philo->id % 2 == 0)
+	if (philo_has_starved(philo))
+		return (PHILO_DIES);
+	if (philo->id % 2)	//? may be the cause of the delay
 	{
-		pthread_mutex_lock(philo->forks[LEFT_FORK]);
 		pthread_mutex_lock(philo->forks[RIGHT_FORK]);
+		pthread_mutex_lock(philo->forks[LEFT_FORK]);
 	}
 	else
 	{
-		pthread_mutex_lock(philo->forks[RIGHT_FORK]);
 		pthread_mutex_lock(philo->forks[LEFT_FORK]);
+		pthread_mutex_lock(philo->forks[RIGHT_FORK]);
 	}
+	if (philo_has_starved(philo))
+		return (PHILO_DIES);
 	write_action(philo, "has taken a fork");
 	write_action(philo, "has taken a fork");
 	write_action(philo, "is eating");
@@ -64,15 +69,27 @@ static int	routine_eat(
 	return (PHILO_LIVES);
 }
 
-static void	routine_sleep(
+static int	routine_sleep(
 	t_philo *philo)
 {
+	if (philo_has_starved(philo))
+		return (PHILO_DIES);
 	write_action(philo, "is sleeping");
 	msleep(*philo->time_to_sleep);
+	return (PHILO_LIVES);
 }
 
-static void	routine_think(
+static int	routine_think(
 	t_philo *philo)
 {
+	if (philo_has_starved(philo))
+		return (PHILO_DIES);
 	write_action(philo, "is thinking");
+	return (PHILO_LIVES);
+}
+
+static int	philo_has_starved(
+	t_philo *philo)
+{
+	return (get_current_timestamp_ms() > philo->timestamp_death);
 }
