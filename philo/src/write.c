94678af40003fd6407fb5deb93beb08ec2f92b50
Sergio <sergio.desantiago@hotmail.es>
2025-10-03 02:02:15 +0200

    refactor: changed write logic, now handled by a single printf call

--- philo/src/write.c
+++ philo/src/write.c
@@ -6,7 +6,7 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 17:41:11 by sede-san          #+#    #+#             */
-/*   Updated: 2025/09/30 14:09:37 by sede-san         ###   ########.fr       */
+/*   Updated: 2025/10/02 23:41:20 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,10 @@ void	write_action(
 	t_philo *philo,
 	char const *action)
 {
-	char	*msg;
-	char	*timestamp;
-	char	*id;
-
-	timestamp = ft_ltoa(get_current_timestamp_ms() - *philo->timestamp_start);
-	id = ft_uitoa(philo->id);
-	msg = ft_strnjoin(6, timestamp, " ", id, " ", action, "\n");
 	pthread_mutex_lock(philo->write_mutex);
-	ft_putstr(msg);
+	printf("%u %u %s\n",
+		get_current_timestamp_ms() - *philo->timestamp_start,
+		philo->id,
+		action);
 	pthread_mutex_unlock(philo->write_mutex);
-	free(timestamp);
-	free(id);
-	free(msg);
 }
